5757a9ffc7577c4f46c6ebfd71937685379ba8ce
Yuri De Martino <ydmartino@gmail.com>
2024-06-15 10:32:13 -0300

    Criação arquivo swagger.js

---
+++ src/swagger.js
@@ -0,0 +1,24 @@
+import swaggerAutogen from 'swagger-autogen'
+
+const doc = {
+    info: {
+        version: "1.0.0",
+        title: "Minha API",
+        description: "API de exemplo - ADS FATEC"
+    },
+
+    servers: [
+        {
+            url: 'http://localhost:3000'
+        }
+        {
+            render: 'https://ling-prog-aula-10.onrender.com'
+        }
+    ]
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFile = ['./server.js'];
+
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFile, doc)
+
